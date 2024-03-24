9908c8ccf4259eb4f47143dec978961510513167
nicolas-leao <nicolaslleao12@gmail.com>
2024-03-24 18:41:28 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+ mario.classList.add('jump');
+
+ setTimeout(() => {
+    mario.classList.remove('jump');
+ }, 500);
+}
+
+const loop = setInterval(() => {
+
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = '/imagens/game-over.png';
+    mario.style.width = '70px'
+    mario.style.marginLeft = '70px'
+
+
+    clearInterval(loop);
+    
+    document.getElementById("botaorei").
+    addEventListener("click", 
+    function(){
+        location.reload();
+        });
+
+}
+
+}, 10);
+
+
+document.addEventListener('keydown', jump);
